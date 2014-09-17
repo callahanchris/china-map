97f1f1d4886ae2e495f17fc259c7f7df1add28a7
Chris Callahan <callahan.c@gmail.com>
2014-09-16 23:55:29 -0400

    extract 7 methods from compute

--- db/seeds.rb
+++ db/seeds.rb
@@ -97,6 +97,17 @@ module RegionAssembler
     @region, @page = region, page
   end
 
+  def compute
+    assign_territorial_designation
+    assign_lat_lng
+    assign_capital
+    assign_area_info
+    assign_gdp
+    assign_gdp_per_cap
+    assign_jvector_code
+    region.save
+  end
+
   def area_info
     @area_info ||= page.search("tr.mergedrow").select {|t| t.text.match(/km2/i) }
   end
@@ -108,8 +119,6 @@ module RegionAssembler
       @monetary_info ||= page.search("tr.mergedrow td").find {|tr| tr.text.match(/cny/i) }.text.split(/\s| |cny|usd|\$/i)
     elsif %w{ Guangdong }.include?(region.name)
       @monetary_info ||= page.search("tr.mergedtoprow td").find {|tr| tr.text.match(/cny/i) }.text.split(/\s| |\$/i)
-    elsif %w{ Hong\ Kong Macau }.include?(region.name)
-      @monetary_info ||= page.search("tr.mergedrow td").select {|tr| tr.text.match(/\$/) }[1].text.split(/\s|\$/)
     else
       @monetary_info ||= page.search("tr.mergedtoprow td").find {|tr| tr.text.match(/cny/i) }.text.split(' ')
     end
@@ -122,14 +131,12 @@ module RegionAssembler
       @gdp_per_cap ||= page.search("tr.mergedrow td").select {|tr| tr.text.match(/cny/i) }.last.text.split(/\s|\$|US/)
     elsif %w{ Tianjin }.include?(region.name)
       @gdp_per_cap ||= page.search("tr.mergedrow td").select {|tr| tr.text.match(/cny/i) }.last.text.split(/\s|\)/)
-    elsif %w{ Hong\ Kong Macau }.include?(region.name)
-      @gdp_per_cap ||= page.search("tr.mergedbottomrow td").select {|tr| tr.text.match(/\$/) }.last.text.split(/\s|\$|\[/)
     else
       @gdp_per_cap ||= page.search("tr.mergedrow td").select {|tr| tr.text.match(/cny/i) }.last.text.split(' ')
     end
   end
 
-  def compute
+  def assign_territorial_designation
     if %w{ Hong\ Kong Macau }.include?(region.name)
       region.territorial_designation = page.search("tr td a").find {|a| a.text.match(/special/i) }.text.split(" of ").first
     else
@@ -137,16 +144,22 @@ module RegionAssembler
     end
 
     region.territorial_designation = region.territorial_designation.split(' ').map(&:capitalize).join(' ')
-    
+  end
+
+  def assign_lat_lng
     if region.name != "Tibet"
       region.latitude = page.search("span.latitude")[0].text
       region.longitude = page.search("span.longitude")[0].text
     end
+  end
 
+  def assign_capital
     if !%w{ Beijing Chongqing Shanghai Tianjin Hong\ Kong Macau }.include?(region.name)
       region.capital = page.search("tr.mergedtoprow a")[0].text
     end
-    
+  end
+
+  def assign_area_info
     if %w{ Hong\ Kong Macau }.include?(region.name)
       region.area_km_sq = area_info.first.text.split(/\s| /)[4].gsub(',', '').to_i
       region.population_density = page.search("tr.mergedbottomrow").select {|t| t.text.match(/km2/i) }.first.text.split(/\s|\[/)[2].gsub(',', '').to_i
@@ -157,7 +170,9 @@ module RegionAssembler
       region.population_density = area_info.last.text.split(/\s| |\//)[3].gsub(',', '').to_i
       region.population = page.search("tr.mergedrow").find {|tr| tr.text.match(/\d{3},\d{3}\n/) }.text.split(' ')[1].gsub(',', '').to_i
     end
+  end
 
+  def assign_gdp
     if %w{ Hong\ Kong Macau }.include?(region.name)
       if monetary_info[2].match(/trillion/)
         region.gdp_usd = (monetary_info[1].to_f * 1_000_000_000_000).to_i
@@ -178,19 +193,22 @@ module RegionAssembler
         region.gdp_usd = (monetary_info[4].to_f * 1_000_000_000).to_i
       end
     end
+  end
 
+  def assign_gdp_per_cap
     if %w{ Hong\ Kong Macau }.include?(region.name)
       region.gdp_per_capita = gdp_per_cap[1].gsub(',', '').to_i
     else
       region.gdp_per_capita = gdp_per_cap[3].gsub(',', '').to_i
     end
+  end
 
+  def assign_jvector_code
     if jvector_keys.include?(region.name)
       region.jvector_code = jvector_codes[region.name]
     end
-
-    region.save
   end
+
 end
 
 class ProvinceAssembler
@@ -207,6 +225,14 @@ end
 
 class SARAssembler
   include RegionAssembler
-end  
+
+  def monetary_info
+    @monetary_info ||= page.search("tr.mergedrow td").select {|tr| tr.text.match(/\$/) }[1].text.split(/\s|\$/)
+  end
+
+  def gdp_per_cap
+    @gdp_per_cap ||= page.search("tr.mergedbottomrow td").select {|tr| tr.text.match(/\$/) }.last.text.split(/\s|\$|\[/)
+  end
+end
 
 ChinaScraper.new
